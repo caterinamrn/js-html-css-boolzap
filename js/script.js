2dea8b7b50197e917dfcb6b044b6c288da669ba2
caterina <catemarian1806@gmail.com>
2020-07-13 16:43:26 +0200

    nuovo messaggio js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
+// function messaggio() {
+//   var messaggio = $("#messaggio").value();
+//   console.log(messaggio);
+// }
+
 
 function init() {
+  var target = $(".chat");
+  console.log(target);
+  $("#messaggio").keypress(function () {
+    // console.log(event.which);
+    if (event.which == 13) {
+    var  messaggio = $("#messaggio").val();
+    target.append("<div class=\"messaggio inviato\">"+ messaggio +"<span class= \"ora\">now</span></div>");
+    $("#messaggio").val("");
+    }
+    console.log(messaggio);
 
+    // target.append( messaggio)
+  })
 }
 
 
