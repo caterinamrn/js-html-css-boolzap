0cdc4d755621e3e7b7ebb07649adcc784d83760f
caterina <catemarian1806@gmail.com>
2020-07-14 18:27:38 +0200

    ricerca contatto

--- js/script.js
+++ js/script.js
@@ -3,30 +3,37 @@
 //   console.log(messaggio);
 // }
 
-function risposta() {
-  setTimeout(function (where) {
-    where.append("<div class=\"messaggio ricevuto\">"+ ok +"<span class= \"ora\">now</span></div>");
-  }, 1000);
-}
-
-
 function init() {
   var target = $(".chat");
-  console.log(target);
+  // console.log(target);
+
+  // search conatti
+  $("#contatto_search").on("keyup",function () {
+    console.log("prova");
+    var value = $(this).val().toLowerCase();
+    console.log(value);
+
+    $("#lista_contatti li").filter(function () {
+      console.log($(this).children(".nome_accesso").children("h4"));
+      $(this).toggle($(this).children(".info_contatto_chat").children(".nome_accesso").children("h4").text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+
+  // scrivi un messaggio e ti arriva una risposta
   $("#messaggio").keypress(function () {
+
     // console.log(event.which);
     if (event.which == 13) {
     var  messaggio = $("#messaggio").val();
     target.append("<div class=\"messaggio inviato\">"+ messaggio +"<span class= \"ora\">now</span></div>");
     $("#messaggio").val("");
-    // risposta(target);
-    setTimeout(function (where) {
-      target.append("<div class=\"messaggio ricevuto\">ok<span class= \"ora\">now</span></div>");
-    }, 1000);
-    }
-    console.log(messaggio);
 
-    // target.append( messaggio)
+    setTimeout( function () {
+      target.append("<div class=\"messaggio ricevuto\"> ok <span class= \"ora\">now</span></div>");
+    } , 3000);
+    }
+
   })
 }
 
